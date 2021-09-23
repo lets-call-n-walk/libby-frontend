37cd813eae903236e942f01c9fad319df86381f7
Walker <collin.walker@capgemini.com>
2021-09-23 13:26:10 -0600

    testing recreation of Angular project

--- libby-frontend/karma.conf.js
+++ libby-frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Front-end'),
+      dir: require('path').join(__dirname, './coverage/libby-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
